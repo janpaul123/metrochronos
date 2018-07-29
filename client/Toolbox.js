0811e80a75cee9c54b22428aed51ef0a8d2be99c
Jan Paul Posma <janpaul123@users.noreply.github.com>
2018-07-28 18:31:44 -0700

    More tweaks

--- client/Toolbox.js
+++ client/Toolbox.js
@@ -1,26 +1,52 @@
 import React from 'react';
 
+import { colorsByHeadway, hoverColorsByHeadway } from './constants';
 import styles from './Toolbox.css';
 
 const blueHeight = 200;
 
+class DraggablePoint extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hovered: false };
+  }
+
+  render() {
+    return (
+      <div
+        className={styles.routePoint}
+        onMouseEnter={() => this.setState({ hovered: true })}
+        onMouseLeave={() => this.setState({ hovered: false })}
+        styles={{
+          background: this.state.hovered
+            ? colorsByHeadway[this.props.headway]
+            : hoverColorsByHeadway[this.props.headway],
+        }}
+      />
+    );
+  }
+}
+
 export default class Toolbox extends React.Component {
+  _renderRoute(headway) {
+    return (
+      <div
+        className={styles.route}
+        style={{ color: colorsByHeadway[headway], height: blueHeight * (headway / 60) }}
+      >
+        <div className={styles.routeLabel}>{headway} min</div>
+        <DraggablePoint headway={headway} />
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className={styles.root}>
         <div className={styles.routeContainer}>
-          <div className={styles.route} style={{ color: '#00f', height: blueHeight }}>
-            <div className={styles.routeLabel}>60 min</div>
-            <div className={styles.routePoint} />
-          </div>
-          <div className={styles.route} style={{ color: '#0d0', height: blueHeight / 2 }}>
-            <div className={styles.routeLabel}>30 min</div>
-            <div className={styles.routePoint} />
-          </div>
-          <div className={styles.route} style={{ color: '#f00', height: blueHeight / 4 }}>
-            <div className={styles.routeLabel}>15 min</div>
-            <div className={styles.routePoint} />
-          </div>
+          {this._renderRoute(60)}
+          {this._renderRoute(30)}
+          {this._renderRoute(15)}
         </div>
       </div>
     );
