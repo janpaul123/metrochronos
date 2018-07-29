499559049d3456830a00e0c526b787cf289afba6
Jan Paul Posma <janpaul123@users.noreply.github.com>
2018-07-29 15:36:51 -0700

    Isochrones basic implementation

--- client/Isochrones.js
+++ client/Isochrones.js
@@ -1,7 +1,35 @@
+import { Feature, Layer } from 'react-mapbox-gl';
 import React from 'react';
+
+const averageWalkingSpeedKph = 5;
+
+const metersToPixelsAtMaxZoom = (meters, latitude) =>
+  meters / 0.075 / Math.cos(latitude * Math.PI / 180);
 
 export default class Isochrones extends React.Component {
   render() {
-    return null;
+    return (
+      <React.Fragment>
+        {this.props.locations.map(({ coordinate, minutesLeft }, index) => {
+          const radiusInMeters = 1000 * averageWalkingSpeedKph * minutesLeft / 60;
+
+          return (
+            <Layer
+              key={index}
+              type="circle"
+              paint={{
+                'circle-radius': {
+                  stops: [[0, 0], [20, metersToPixelsAtMaxZoom(radiusInMeters, coordinate[1])]],
+                  base: 2,
+                },
+                'circle-color': '#ff0',
+              }}
+            >
+              <Feature coordinates={coordinate} />
+            </Layer>
+          );
+        })}
+      </React.Fragment>
+    );
   }
 }
