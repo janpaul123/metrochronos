1ab92ac34c76bf5cd8bb9f9831da2f823595ccaf
Jan Paul Posma <janpaul123@users.noreply.github.com>
2018-07-29 15:52:02 -0700

    Fix entry

--- client/entry.js
+++ client/entry.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Main from './Main';
-import makeBusStops from './makeBusStops';
-import makeBusGraph from './makeBusGraph';
 
 let metrochronosData = JSON.parse(localStorage.metrochronosData || '{}');
 metrochronosData.routes = metrochronosData.routes || {};
@@ -14,12 +12,10 @@ document.body.appendChild(element);
 function render() {
   ReactDOM.render(
     <Main
-      data={JSON.parse(localStorage.metrochronosData)}
+      data={metrochronosData}
       onChange={data => {
         metrochronosData = data;
         localStorage.metrochronosData = JSON.stringify(metrochronosData);
-        console.log(data.pinLocation);
-        makeBusGraph(Object.values(data.routes).map(makeBusStops));
         render();
       }}
     />,
