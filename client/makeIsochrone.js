f5f5987ee704f94fb52634725c061829ba4723d7
Jan Paul Posma <janpaul123@users.noreply.github.com>
2018-07-29 15:50:22 -0700

    Remove dummy isochrone

--- client/makeIsochrone.js
+++ client/makeIsochrone.js
@@ -1,3 +1,4 @@
 export default function makeIsochrone(data) {
-  return [{ coordinate: [-122.39707304059863, 37.785571889585015], minutesLeft: 40 }];
+  // return [{ coordinate: [-122.39707304059863, 37.785571889585015], minutesLeft: 40 }];
+  return [];
 }
